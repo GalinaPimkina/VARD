e2440b52615782c3f4f4347eb7892d1e66782c98
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-03-15 13:13:34 +0300

    модель User переопределена через AbstractUser

--- vard/users/models.py
+++ vard/users/models.py
@@ -1,1 +1,8 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_passwords_change = models.DateTimeField(auto_now=True)
