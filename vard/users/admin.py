36dea901b7b85cd36b0315854e18add1ff823a73
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-03-15 18:59:45 +0300

    .

--- vard/users/admin.py
+++ vard/users/admin.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from .models import User
-
-@admin.register(User)
-class AdminUser(admin.ModelAdmin):
-    list_display = ['username', 'name', 'email', 'date_creation', 'last_login', 'is_superuser']
-    search_fields = ['username', 'name', 'email']
+# from django.contrib import admin
+# from .models import User
+#
+# @admin.register(User)
+# class AdminUser(admin.ModelAdmin):
+#     list_display = ['username', 'name', 'email', 'date_creation', 'last_login', 'is_superuser']
+#     search_fields = ['username', 'name', 'email']
