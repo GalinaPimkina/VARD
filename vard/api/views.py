dfd654a65c94365fe1cb3f8d7f68e4b3597c2b7a
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-03-21 16:50:27 +0300

    add ModelViewSet - AccessViewSet, FileViewSet

--- vard/api/views.py
+++ vard/api/views.py
@@ -1,11 +1,23 @@
 from rest_framework import viewsets
 from rest_framework import permissions
 
-from api.serializers import UserSerializer
-from users.models import User
+from api.serializers import UserSerializer, AccessSerializer, FileSerializer
+from users.models import User, Access, File
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_creation')
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class AccessViewSet(viewsets.ModelViewSet):
+    queryset = Access.objects.all()
+    serializer_class = AccessSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
     permission_classes = [permissions.IsAuthenticated]
