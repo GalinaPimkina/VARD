08a90098e2cbbbb82467294283d4ff0ff21dbf81
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-03-21 15:58:58 +0300

    add class UserViewSet

--- vard/api/views.py
+++ vard/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
 
-# Create your views here.
+from api.serializers import UserSerializer
+from users.models import User
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_creation')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
