a70e63810733fe32366777261643966d9c2c0495
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-03-21 16:50:00 +0300

    add serializers AccessSerializer, FileSerializer

--- vard/api/serializers.py
+++ vard/api/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from users.models import User
+from users.models import User, Access, File
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+
+
+class AccessSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Access
+        fields = ['user_id', 'file_id', 'access_type']
+
+
+class FileSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = File
+        fields = ['name', 'link', 'publish', 'type']
