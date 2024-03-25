ace45d11d4e7f049926bcba0e4c2309431daba36
GalinaPimkina <133103137+GalinaPimkina@users.noreply.github.com>
2024-03-25 15:01:51 +0300

    delete permission_classes from all viewsets

--- vard/api/views.py
+++ vard/api/views.py
@@ -24,51 +24,40 @@ from users.models import (
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    # queryset = User.objects.all().order_by("-date_creation")
+    queryset = User.objects.all().order_by("-date_creation")
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get_queryset(self):
-        return User.objects.all()
 
 
 class AccessViewSet(viewsets.ModelViewSet):
     queryset = Access.objects.all()
     serializer_class = AccessSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class FileViewSet(viewsets.ModelViewSet):
     queryset = File.objects.all()
     serializer_class = FileSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class DashboardViewSet(viewsets.ModelViewSet):
     queryset = Dashboard.objects.all()
     serializer_class = DashboardSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class ChartViewSet(viewsets.ModelViewSet):
     queryset = Chart.objects.all()
     serializer_class = ChartSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class FeedbackViewSet(viewsets.ModelViewSet):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
 
 
 class ReadCommentViewSet(viewsets.ModelViewSet):
     queryset = ReadComment.objects.all()
     serializer_class = ReadCommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
